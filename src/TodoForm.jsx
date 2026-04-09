531c093304b878c2d19beabd02b5ad3e49c5d8cf
Justin <justinjones38@myyahoo.com>
2026-04-09 15:31:02 -0500

    Completed lesson-02 details

---
+++ src/TodoForm.jsx
@@ -0,0 +1,9 @@
+export default function TodoForm() {
+  return (
+    <form>
+      <label htmlFor="todoTitle">Todo</label>
+      <input type="text" id="todoTitle" />
+      <button type="submit" disabled>Add Todo</button>
+    </form>
+  );
+}
