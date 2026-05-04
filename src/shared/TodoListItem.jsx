f907bc4187ea2c90d707ecf22a9d92fc3be951b5
Justin <justinjones38@myyahoo.com>
2026-05-04 07:47:10 -0500

    Consolidated editing loginc in a custom hook

--- src/shared/TodoListItem.jsx
+++ src/shared/TodoListItem.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import TextInputWithLabel from "./TextInputWithLabel";
 import { isValidTodoTitle } from "../utils/todoValidation";
+import useEditableTitle from "../hooks/useEditableTitle";
 
 export default function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [workingTitle, setWorkingTitle] = useState(todo.title);
+  const {isEditing, workingTitle, startEditing, cancelEdit, updateTitle, finishEdit} = useEditableTitle(todo.title);
+
+
   const handleCancel = () => {
-    setWorkingTitle(todo.title);
-    setIsEditing(false);
+    cancelEdit();
   };
 
   const handleEdit = event => {
-    setWorkingTitle(event.target.value)
+    updateTitle(event.target.value)
   }
 
   const handleUpdate = event => {
@@ -19,8 +20,8 @@ export default function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
       return;
     }
     event.preventDefault();
-    onUpdateTodo({...todo, title: workingTitle})
-    setIsEditing(false);
+    const finalTitle = finishEdit();
+    onUpdateTodo({...todo, title: finalTitle})
   }
  
   return (
@@ -42,7 +43,7 @@ export default function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
                 onChange={() => onCompleteTodo(todo.id)}
               />
             </label>
-            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+            <span onClick={() => startEditing()}>{todo.title}</span>
           </>
         )}
       </form>
